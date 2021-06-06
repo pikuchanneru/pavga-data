7076fa2dcd0da36a8c5fe8a2f1381fafcca0516b
tawiradarmo <tawiradarmo@gmail.com>
2021-06-06 16:18:18 +0700

    Update pokemon.sql
    Join all metrics into one big query.

--- query-info/pokemon.sql
+++ query-info/pokemon.sql
@@ -1,5 +1,6 @@
 WITH teams AS (
     SELECT
+        tournament_id,
         team,
         two_nil + two_one AS win,
         nil_two + one_two + double_loss AS loss,
@@ -11,6 +12,7 @@ WITH teams AS (
 roster AS (
     SELECT
         unnest(team) AS pokemon,
+        tournament_id,
         win,
         loss,
         draw,
@@ -18,62 +20,83 @@ roster AS (
     FROM
         teams
 ),
-usage AS (
-    SELECT
-        pokemon,
-        count(pokemon) AS count,
-        round(count(pokemon) / 41 :: DECIMAL, 4) AS "share"
-    FROM
-        roster
-    GROUP BY
-        pokemon
-),
-winrate AS (
-    SELECT
-        pokemon,
-        sum(win) AS win,
-        sum(loss) AS loss,
-        sum(draw) AS draw,
-        round(
-            sum(win) /(sum(win) + sum(loss) + sum(draw)) :: DECIMAL,
-            4
-        ) AS winrate
-    FROM
-        roster
-    GROUP BY
-        pokemon
-),
-ppt AS (
+headcount AS (
     SELECT
         tournament_id,
-        SUM(point_award)
+        COUNT(*) AS count
     FROM
         standing
     GROUP BY
         tournament_id
 ),
-ptrate AS (
+ptsum AS (
     SELECT
+        tournament_id,
+        SUM(point_award) as sum
+    FROM
+        standing
+    GROUP BY
+        tournament_id
+),
+usage AS (
+    SELECT
+        tournament_id,
+        pokemon,
+        count(pokemon) AS count
+    FROM
+        roster
+    GROUP BY
+        tournament_id,
+        pokemon
+),
+result AS (
+    SELECT
+        tournament_id,
+        pokemon,
+        sum(win) AS win,
+        sum(loss) AS loss,
+        sum(draw) AS draw,
+        sum(win) + sum(loss) + sum(draw) as match_played
+    FROM
+        roster
+    GROUP BY
+        tournament_id,
+        pokemon
+),
+reward AS (
+    SELECT
+        tournament_id,
         pokemon,
         sum(point_award) AS pt
     FROM
         roster
     GROUP BY
+        tournament_id,
         pokemon
 )
 SELECT
+    uu.tournament_id,
     uu.pokemon,
     uu.count,
-    uu.share,
-    ww.win,
-    ww.loss,
-    ww.draw,
-    ww.winrate
+    round(uu.count / hc.count :: DECIMAL, 4) AS "usage_share",
+    rr.win,
+    rr.draw,
+    rr.loss,
+    round(
+        rr.win / rr.match_played :: DECIMAL,
+        4
+    ) AS winrate,
+    rw.pt,
+    round(rw.pt / ps.sum :: DECIMAL, 4) AS "pt_share"
 FROM
     usage uu
-    LEFT JOIN winrate ww ON ww.pokemon = uu.pokemon
+    LEFT JOIN result rr ON uu.tournament_id = rr.tournament_id
+    AND uu.pokemon = rr.pokemon
+    LEFT JOIN headcount hc ON uu.tournament_id = hc.tournament_id
+    LEFT JOIN reward rw ON uu.tournament_id = rw.tournament_id
+    AND uu.pokemon = rw.pokemon
+    LEFT JOIN ptsum ps ON uu.tournament_id = ps.tournament_id
+WHERE
+    uu.tournament_id = 1
 ORDER BY
-    uu.count DESC,
-    ww.winrate DESC,
-    ww.win DESC,
-    ww.loss ASC
+    usage_share DESC
