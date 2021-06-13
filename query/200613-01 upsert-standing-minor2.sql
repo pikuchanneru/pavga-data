6d877c6ae072f793e230599d825c746e8cb6781d
Taufik Wiradarmo <taufikwiradarmo@gmail.com>
2021-06-13 20:03:40 +0700

    Create 200613-01 upsert-standing-minor2.sql

---
+++ query/200613-01 upsert-standing-minor2.sql
@@ -0,0 +1,112 @@
+INSERT INTO
+    standing (
+        tournament_id
+    ,trainer_id
+    ,team
+    ,team_super
+    ,two_one
+    ,two_nil
+    ,one_two
+    ,nil_two
+    ,double_loss
+    ,bye
+    ,draw
+    ,swiss
+    ,dnf
+    ,finish
+    ,point_award
+    ,prize_award
+    ,prize_currency
+    ,created_by
+    ,updated_by
+    )
+VALUES
+(2,36,'{Togekiss,Landorus-Therian,Regieleki,Incineroar,Tapu Fini,Urshifu-SS}','{Togekiss,Landorus-Therian,Regieleki,Incineroar,Tapu Fini,Urshifu-SS}',5,5,0,1,0,0,0,3,FALSE,1,16,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,44,'{Araquanid ,Dusclops,Rillaboom,Indeedee-F,Urshifu-RS,Zapdos}','{Araquanid ,Dusclops,Rillaboom,Indeedee-F,Urshifu-RS,Zapdos}',5,3,2,1,0,0,0,8,FALSE,2,11,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,7,'{Dragapult,Togekiss,Urshifu-RS,Regieleki,Incineroar,Amoonguss}','{Dragapult,Togekiss,Urshifu-RS,Regieleki,Incineroar,Amoonguss}',4,4,0,2,0,0,0,4,FALSE,3,10,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,80,'{Grimmsnarl,Venusaur,Torkoal,Landorus-Therian,Registeel,Porygon2}','{Grimmsnarl,Venusaur,Torkoal,Landorus-Therian,Registeel,Porygon2}',1,6,2,1,0,0,0,7,FALSE,4,10,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,79,'{Coalossal,Dragapult,Rillaboom,Urshifu-RS,Incineroar,Togekiss}','{Coalossal,Dragapult,Rillaboom,Urshifu-RS,Incineroar,Togekiss}',3,4,1,0,0,1,0,1,FALSE,5,8,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,81,'{Regieleki,Entei,Moltres-Galar,Landorus-Therian,Grimmsnarl,Registeel}','{Regieleki,Entei,Moltres-Galar,Landorus-Therian,Grimmsnarl,Registeel}',3,3,1,1,0,1,0,2,FALSE,6,8,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,50,'{Spectrier,Clefairy,Regieleki,Urshifu-RS,Incineroar,Landorus-Therian}','{Spectrier,Clefairy,Regieleki,Urshifu-RS,Incineroar,Landorus-Therian}',5,1,1,2,0,0,0,11,FALSE,7,8,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,54,'{Comfey,Incineroar,Spectrier,Rillaboom,Metagross,Garchomp}','{Comfey,Incineroar,Spectrier,Rillaboom,Metagross,Garchomp}',2,4,2,1,0,0,0,12,FALSE,8,8,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,77,'{Urshifu-SS,Thundurus,Landorus-Therian,Tapu Fini,Regieleki,Incineroar}','{Urshifu-SS,Thundurus,Landorus-Therian,Tapu Fini,Regieleki,Incineroar}',4,1,1,2,0,0,0,5,FALSE,9,6,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,58,'{Sylveon,Moltres-Galar,Porygon2,Marowak-Alola,Blastoise,Kartana}','{Sylveon,Moltres-Galar,Porygon2,Marowak-Alola,Blastoise,Kartana}',3,2,1,2,0,0,0,6,FALSE,10,6,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,51,'{Indeedee-F,Metagross,Torkoal,Hatterene,Venusaur,Regieleki}','{Indeedee-F,Metagross,Torkoal,Hatterene,Venusaur,Regieleki}',1,4,1,2,0,0,0,9,FALSE,11,6,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,61,'{Coalossal,Dragapult,Regigigas,Rillaboom,Urshifu-RS,Weezing}','{Coalossal,Dragapult,Regigigas,Rillaboom,Urshifu-RS,Weezing}',2,3,1,2,0,0,0,10,FALSE,12,6,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,84,'{Regigigas,Regieleki,Weezing,Urshifu-SS,Spectrier,Nihilego}','{Regigigas,Regieleki,Weezing,Urshifu-SS,Spectrier,Nihilego}',3,2,2,1,0,0,0,13,FALSE,13,6,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,67,'{Regieleki,Urshifu-SS,Thundurus,Tapu Fini,Landorus-Therian,Incineroar}','{Regieleki,Urshifu-SS,Thundurus,Tapu Fini,Landorus-Therian,Incineroar}',4,1,3,0,0,0,0,14,FALSE,14,6,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,3,'{Mamoswine,Grimmsnarl,Incineroar,Tapu Fini,Moltres-Galar,Registeel}',NULL,3,1,3,0,0,0,0,15,FALSE,15,6,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,65,'{Venusaur,Torkoal,Zapdos,Regirock,Porygon2,Grimmsnarl}','{Venusaur,Torkoal,Zapdos,Regirock,Porygon2,Grimmsnarl}',2,2,1,2,0,0,0,16,FALSE,16,6,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,20,'{Regirock,Registeel,Rotom-Wash,Dragapult,Togekiss,Incineroar}',NULL,1,3,0,3,0,0,0,17,FALSE,17,4,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,63,'{Porygon2,Conkeldurr,Zapdos,Tapu Fini,Landorus-Therian,Incineroar}',NULL,0,4,1,2,0,0,0,18,FALSE,18,4,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,75,'{Excadrill,Tyranitar,Tapu Fini,Clefairy,Dragapult,Rotom-Heat}',NULL,1,3,3,0,0,0,0,19,FALSE,19,4,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,70,'{Regieleki,Whimsicott,Urshifu-SS,Tapu Lele,Landorus-Therian,Lapras}','{Regieleki,Whimsicott,Urshifu-SS,Tapu Lele,Landorus-Therian,Lapras}',2,2,3,0,0,0,0,20,FALSE,20,4,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,39,'{Incineroar,Exeggutor-Alola,Stakataka,Glastrier,Indeedee-F,Slowking-Galar}',NULL,2,2,1,2,0,0,0,21,FALSE,21,4,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,27,'{Whimsicott,Celesteela,Landorus,Incineroar,Marowak-Alola,Urshifu-RS}',NULL,2,2,2,1,0,0,0,22,FALSE,22,4,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,23,'{Incineroar,Celesteela,Comfey,Mamoswine,Thundurus-Therian,Tapu Koko}',NULL,3,1,2,1,0,0,0,23,FALSE,23,4,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,83,'{Metagross,Spectrier,Grimmsnarl,Tapu Fini,Landorus-Therian,Rotom-Heat}',NULL,3,1,2,1,0,0,0,24,FALSE,24,4,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,49,'{Spectrier,Clefairy,Regieleki,Urshifu-RS,Incineroar,Landorus-Therian}','{Spectrier,Clefairy,Regieleki,Urshifu-RS,Incineroar,Landorus-Therian}',3,1,0,3,0,0,0,25,FALSE,25,4,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,60,'{Garchomp,Cinderace,Rillaboom,Lapras,Raichu,Celesteela}','{Garchomp,Cinderace,Rillaboom,Lapras,Raichu,Celesteela}',1,2,2,2,0,0,0,26,FALSE,26,4,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,31,'{Porygon2,Conkeldurr,Zapdos,Tapu Fini,Landorus-Therian,Incineroar}',NULL,2,1,1,3,0,0,0,27,FALSE,27,4,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,85,'{Urshifu-SS,Thundurus,Stakataka,Clefairy,Cinderace,Togekiss}','{Urshifu-SS,Thundurus,Stakataka,Clefairy,Cinderace,Togekiss}',2,1,4,0,0,0,0,28,FALSE,28,4,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,56,'{Darmanitan-Galar,Indeedee-M,Coalossal,Torkoal,Charizard,Azumarill}','{Darmanitan-Galar,Indeedee-M,Coalossal,Torkoal,Charizard,Azumarill}',0,3,3,1,0,0,0,29,FALSE,29,4,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,12,'{Clefairy,Spectrier,Regieleki,Urshifu-SS,Glastrier,Tapu Fini}','{Clefairy,Spectrier,Regieleki,Urshifu-SS,Glastrier,Tapu Fini}',0,3,3,1,0,0,0,30,FALSE,30,4,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,25,'{Regieleki,Garchomp,Tapu Fini,Incineroar,Umbreon,Celesteela}',NULL,2,1,0,4,0,0,0,31,FALSE,31,4,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,24,'{Gothitelle,Regieleki,Rillaboom,Urshifu-RS,Landorus-Therian,Togekiss}','{Gothitelle,Regieleki,Rillaboom,Urshifu-RS,Landorus-Therian,Togekiss}',1,2,3,1,0,0,0,32,FALSE,32,4,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,35,'{Landorus-Therian,Tapu Fini,Thundurus,Stakataka,Incineroar,Cresselia}',NULL,2,1,2,2,0,0,0,33,FALSE,33,2,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,73,'{Kingdra,Thundurus,Rillaboom,Mimikyu,Cobalion,Incineroar}',NULL,0,2,1,3,0,1,0,34,FALSE,34,2,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,66,'{Rillaboom,Cinderace,Rotom-Wash,Ferrothorn,Togekiss,Naganadel}',NULL,0,2,2,3,0,0,0,35,FALSE,35,2,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,48,'{Landorus-Therian,Tapu Fini,Glastrier,Porygon2,Regieleki,Celesteela}',NULL,1,1,3,1,1,0,0,36,FALSE,36,2,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,59,'{Snorlax,Rotom-Heat,Togekiss,Indeedee-F,Urshifu-SS,Kartana}','{Snorlax,Rotom-Heat,Togekiss,Indeedee-F,Urshifu-SS,Kartana}',0,0,2,2,2,1,0,37,FALSE,37,2,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,1,'{Regieleki,Urshifu-SS,Thundurus,Tapu Fini,Landorus-Therian,Incineroar}','{Regieleki,Urshifu-SS,Thundurus,Tapu Fini,Landorus-Therian,Incineroar}',3,0,1,2,0,0,0,38,TRUE,38,1,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,16,'{Azumarill,Coalossal,Rillaboom,Incineroar,Gothitelle,Glastrier}','{Azumarill,Coalossal,Rillaboom,Incineroar,Gothitelle,Glastrier}',1,2,0,1,0,0,0,39,TRUE,39,0,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,72,'{Venusaur,Celesteela,Urshifu-SS,Landorus-Therian,Grimmsnarl,Regieleki}','{Venusaur,Celesteela,Urshifu-SS,Landorus-Therian,Grimmsnarl,Regieleki}',3,0,2,1,0,0,0,40,TRUE,40,1,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,76,'{Grimmsnarl,Landorus-Therian,Venusaur,Celesteela,Rotom-Wash,Heatran}','{Grimmsnarl,Landorus-Therian,Venusaur,Celesteela,Rotom-Wash,Heatran}',1,2,0,3,0,0,0,41,TRUE,41,1,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,71,'{Charizard,Whimsicott,Tapu Lele,Urshifu-SS,Mamoswine,Regieleki}','{Charizard,Whimsicott,Tapu Lele,Urshifu-SS,Mamoswine,Regieleki}',0,2,1,2,0,0,0,42,TRUE,42,1,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,46,'{Regice,Thundurus,Celesteela,Bronzong,Kangaskhan,Landorus-Therian}',NULL,1,1,1,0,0,0,0,43,TRUE,43,0,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,62,'{Clefairy,Glastrier,Urshifu-RS,Rillaboom,Regieleki,Celesteela}','{Clefairy,Glastrier,Urshifu-RS,Rillaboom,Regieleki,Celesteela}',0,2,1,3,0,0,0,44,TRUE,44,1,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,15,'{Marowak-Alola,Thundurus,Blastoise,Celesteela,Garchomp,Clefairy}',NULL,0,2,2,2,0,0,0,45,TRUE,45,1,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,68,'{Dragapult,Coalossal,Urshifu-RS,Rillaboom,Incineroar,Togekiss}','{Dragapult,Coalossal,Urshifu-RS,Rillaboom,Incineroar,Togekiss}',0,1,0,2,0,0,0,46,TRUE,46,0,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,26,'{Moltres-Galar,Registeel,Grimmsnarl,Spectrier,Rotom-Wash,Garchomp}','{Moltres-Galar,Registeel,Grimmsnarl,Spectrier,Rotom-Wash,Garchomp}',1,0,2,1,0,0,0,47,TRUE,47,0,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,57,'{Lycanroc-Midday,Tyranitar,Cobalion,Dragapult,Spectrier,Whimsicott}',NULL,0,0,1,2,0,1,0,48,TRUE,48,0,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,11,'{Spectrier,Clefairy,Urshifu-RS,Regieleki,Glastrier,Rillaboom}','{Spectrier,Clefairy,Urshifu-RS,Regieleki,Glastrier,Rillaboom}',0,1,1,1,0,0,0,49,TRUE,49,0,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,28,'{Landorus-Therian,Regieleki,Grimmsnarl,Registeel,Spectrier,Moltres-Galar}',NULL,0,1,1,2,0,0,0,50,TRUE,50,0,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,32,'{Regieleki,Glastrier,Celesteela,Urshifu-SS,Clefairy,Rillaboom}','{Regieleki,Glastrier,Celesteela,Urshifu-SS,Clefairy,Rillaboom}',0,1,1,2,0,0,0,51,TRUE,51,0,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,53,'{Spectrier,Clefairy,Urshifu-RS,Regieleki,Glastrier,Rillaboom}','{Spectrier,Clefairy,Urshifu-RS,Regieleki,Glastrier,Rillaboom}',1,0,1,4,0,0,0,52,TRUE,52,1,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,43,'{Porygon-Z,Clefairy,Kartana,Regieleki,Urshifu-SS,Landorus-Therian}','{Porygon-Z,Clefairy,Kartana,Regieleki,Urshifu-SS,Landorus-Therian}',0,1,2,1,0,0,0,53,TRUE,53,0,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,82,'{Dragapult,Togekiss,Urshifu-RS,Incineroar,Rillaboom,Coalossal}','{Dragapult,Togekiss,Urshifu-RS,Incineroar,Rillaboom,Coalossal}',0,1,3,1,1,0,0,54,TRUE,54,1,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,42,'{Landorus-Therian,Moltres-Galar,Grimmsnarl,Kartana,Tapu Fini,Rotom-Heat}','{Landorus-Therian,Moltres-Galar,Grimmsnarl,Kartana,Tapu Fini,Rotom-Heat}',0,0,1,1,2,1,0,55,TRUE,55,1,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,69,'{Glastrier,Clefairy,Regieleki,Urshifu-RS,Spectrier,Rillaboom}','{Glastrier,Clefairy,Regieleki,Urshifu-RS,Spectrier,Rillaboom}',0,1,0,3,1,0,0,56,TRUE,56,1,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,78,'{Charizard,Mamoswine,Regieleki,Landorus-Therian,Clefairy,Torkoal}','{Charizard,Mamoswine,Regieleki,Landorus-Therian,Clefairy,Torkoal}',0,0,2,0,0,0,0,57,TRUE,57,0,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,55,'{Regigigas,Weezing,Urshifu-SS,Tapu Fini,Tornadus,Rotom-Heat}',NULL,0,0,0,2,0,0,0,58,TRUE,58,0,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,45,'{Gothitelle,Glastrier,Hatterene,Incineroar,Porygon2,Marowak-Alola}','{Gothitelle,Glastrier,Hatterene,Incineroar,Porygon2,Marowak-Alola}',0,0,0,3,0,0,0,59,TRUE,59,0,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,64,'{Spectrier,Indeedee-F,Regieleki,Urshifu-RS,Whimsicott,Terrakion}','{Spectrier,Indeedee-F,Regieleki,Urshifu-RS,Whimsicott,Terrakion}',0,0,1,2,0,0,0,60,TRUE,60,0,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,13,'{Glastrier,Incineroar,Regieleki,Clefairy,Porygon-Z,Urshifu-RS}','{Glastrier,Incineroar,Regieleki,Clefairy,Porygon-Z,Urshifu-RS}',0,0,0,1,1,0,0,61,TRUE,61,0,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,47,'{Cinderace,Charizard,Venusaur,Whimsicott,Jynx,Duraludon}','{Cinderace,Charizard,Venusaur,Whimsicott,Jynx,Duraludon}',0,0,0,2,0,0,0,62,TRUE,62,0,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,52,'{Clefairy,Urshifu-SS,Celesteela,Glastrier,Regieleki,Rillaboom}','{Clefairy,Urshifu-SS,Celesteela,Glastrier,Regieleki,Rillaboom}',0,0,1,1,1,0,0,63,TRUE,63,0,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,9,'{Armaldo,Golisopod,Pelipper,Appletun,Ferrothorn,Raichu}',NULL,0,0,0,2,0,0,0,64,TRUE,64,0,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,74,'{Regieleki,Clefairy,Rillaboom,Urshifu-SS,Glastrier,Celesteela}','{Regieleki,Clefairy,Rillaboom,Urshifu-SS,Glastrier,Celesteela}',0,0,0,1,1,0,0,65,TRUE,65,0,NULL,NULL,'pikuchanneru','pikuchanneru'),
+(2,8,'{Metagross,Kingdra,Politoed,Latias,Rillaboom,Tapu Koko}','{Metagross,Kingdra,Politoed,Latias,Rillaboom,Tapu Koko}',0,0,0,0,2,0,0,66,TRUE,66,0,NULL,NULL,'pikuchanneru','pikuchanneru')
+    
+    ON CONFLICT (id) DO
+UPDATE
+SET
+    tournament_id = EXCLUDED.tournament_id
+    ,trainer_id = EXCLUDED.trainer_id
+    ,team = EXCLUDED.team
+    ,team_super = EXCLUDED.team_super
+    ,two_one = EXCLUDED.two_one
+    ,two_nil = EXCLUDED.two_nil
+    ,one_two = EXCLUDED.one_two
+    ,nil_two = EXCLUDED.nil_two
+    ,double_loss = EXCLUDED.double_loss
+    ,bye = EXCLUDED.bye
+    ,draw = EXCLUDED.draw
+    ,swiss = EXCLUDED.swiss
+    ,dnf = EXCLUDED.dnf
+    ,finish = EXCLUDED.finish
+    ,point_award = EXCLUDED.point_award
+    ,prize_award = EXCLUDED.prize_award
+    ,prize_currency = EXCLUDED.prize_currency
+    ,created_by = EXCLUDED.created_by,
+    updated_by = EXCLUDED.updated_by;
