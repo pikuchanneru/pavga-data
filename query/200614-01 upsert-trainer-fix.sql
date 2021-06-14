c9b871982e31fb010d3ac7a5c0e321ec1c4e815d
Taufik Wiradarmo <taufikwiradarmo@gmail.com>
2021-06-14 09:30:11 +0700

    Update 200614-01 upsert-trainer-fix.sql

--- query/200614-01 upsert-trainer-fix.sql
+++ query/200614-01 upsert-trainer-fix.sql
@@ -1,26 +1,28 @@
 INSERT INTO
     trainer (
         id,
-        twitter
+        discord,
+        battlefy,
+        twitter,
+        realname,
+        nickname,
+        country,
+        created_by,
+        updated_by
     )
 VALUES
-(44,'anabelvg_21')
+(44,'Mkh#3748','alvinsoyol','anabelvg_21','Mak Kwan Ho','AnabelVg','HK','pikuchanneru','pikuchanneru'),
+(77,'Kassiddy#0974','kischnp','Kassiddy_VGC','Kisch Cheng','Kassiddy','HK','pikuchanneru','pikuchanneru'),
+(55,'IBOIII#1111','dalisebastian06','notIBOIII','Dali Sebastian Putra Noviyanto','IBOIII','ID','pikuchanneru','pikuchanneru')
     
      ON CONFLICT (id) DO
 UPDATE
 SET
-    twitter = EXCLUDED.twitter;
-
-INSERT INTO
-    trainer (
-        id,
-        realname
-    )
-VALUES
-(77,'Kisch Cheng'),
-(55,'Dali Sebastian Putra Noviyanto')
-    
-     ON CONFLICT (id) DO
-UPDATE
-SET
-    realname = EXCLUDED.realname;
+    discord = EXCLUDED.discord,
+    battlefy = EXCLUDED.battlefy,
+    twitter = EXCLUDED.twitter,
+    realname = EXCLUDED.realname,
+    nickname = EXCLUDED.nickname,
+    country = EXCLUDED.country,
+    created_by = EXCLUDED.created_by,
+    updated_by = EXCLUDED.updated_by;
